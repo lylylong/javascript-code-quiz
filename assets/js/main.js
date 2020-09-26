6bbc5538aae49168d67cf6ae01d2b09b35822302
Lyly Long <lylylong@gmail.com>
2020-09-26 16:35:51 -0400

    setup timer and debug

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,19 +8,42 @@ let currentQuestionChoices = document.querySelector("#choices-div");
 let eachChoice = document.querySelector("#choice");
 let currentQuestionIndex = 0;
 
-//TIMER
-let timeAll = questions.length * 15;
+//QUIZ DONE
+let quizDoneDiv = document.getElementById("quiz-done");
+let finalScore = document.getElementById("final-score");
+
+//TIME
+let timeAll = 100;
+console.log(questions.length * 10);
 let timeLeft = document.getElementById("time-left");
-let timerId;
+let timer;
 
 //FEEDBACK
 let feedbackDiv = document.getElementById("feedback-div");
 let feedbackResult = document.getElementById("feedback");
 let nextBtn = document.getElementById("next-btn");
 
+//the process starts here:
+//////////////
+//time changing
+function clockTick() {
+  timeAll--;
+  timeLeft.textContent = timeAll;
+  if (timeAll <= 0) {
+    quizDonePage();
+  }
+  console.log(timeAll);
+}
+
+//////////////
+//////////////
+
 //JUMP TO QUESTION PAGE
 function showQuestionsPage() {
-  console.log("Question Page" + currentQuestionIndex);
+  //START TIMER
+  timer = setInterval(clockTick, 1000);
+  //starting time
+  timeLeft.textContent = timeAll;
 
   //hide intro page
   let intro = document.getElementById("intro");
@@ -30,6 +53,7 @@ function showQuestionsPage() {
   quizDiv.removeAttribute("class");
 
   //show questions
+  console.log("Question Page " + currentQuestionIndex + 1);
   let currentQuestion = questions[currentQuestionIndex];
   currentQuestionTitle.textContent = currentQuestion.title;
 
@@ -53,24 +77,43 @@ function showQuestionsPage() {
 ///////////
 //current question feedback
 function questionFeedback() {
+  timeLeft.textContent = timeAll;
+
+  //if the choice is wrong
   if (this.value !== questions[currentQuestionIndex].answer) {
     feedbackResult.textContent = "😔 Wrong";
     //release feedback
     feedbackDiv.removeAttribute("class");
-  } else {
+    timeAll -= 15;
+    timeLeft.textContent = timeAll;
+  }
+  //if the choice is correct
+  else {
     feedbackResult.textContent = "👍 Correct!";
     //release feedback
     feedbackDiv.removeAttribute("class");
   }
 
-  nextBtn.onclick = moreQuestions;
+  //test if all questions are done
+  if (currentQuestionIndex === questions.length - 1) {
+    nextBtn.onclick = quizDonePage;
+  } else {
+    nextBtn.onclick = moreQuestions;
+  }
 }
 
 function moreQuestions() {
+  timeLeft.textContent = timeAll;
+  if (timeLeft <= 0) {
+    quizDonePage();
+  }
   //hide the feedback again
   feedbackDiv.setAttribute("class", "hide");
   //move to next question
   currentQuestionIndex++;
+  /////////////////////////////////////////
+  ///////will put into a function/////////
+  console.log("Question Page " + currentQuestionIndex);
   //show questions
   let currentQuestion = questions[currentQuestionIndex];
   currentQuestionTitle.textContent = currentQuestion.title;
@@ -88,12 +131,31 @@ function moreQuestions() {
     //click on one of the choices, will show current question feedback
     eachChoiceBtn.onclick = questionFeedback;
   });
+  ///////will put into a function/////////
+  /////////////////////////////////////////
+}
+
+function quizDonePage() {
+  //end timer
+  clearInterval(timer);
+
+  //hide the feedback again
+  feedbackDiv.setAttribute("class", "hide");
+  //hide the questions
+  quizDiv.setAttribute("class", "hide");
+  //show the quiz done div
+  quizDoneDiv.removeAttribute("class");
+
+  //show final score, chnage to timeleft * 5
+  finalScore.textContent = questions.length * 20;
+  console.log("the final score is " + questions.length * 20);
 }
 
 //RECEIVE CLICK - PROCERSS TO QUESTION PAGE
 let startQuizHandler = function (event) {
   event.preventDefault();
   console.log("Now the quiz starts");
+
   showQuestionsPage();
 };
 
